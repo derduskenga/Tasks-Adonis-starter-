4dc6d6c8e556464510bb005c40f43b8eb2516c22
Kerdus Kenga <derduskenga@gmail.com>
2020-11-02 17:23:12 +0300

    views layout set up and mysql database setup as well

---
+++ app/Controllers/Http/PagesController.ts
@@ -0,0 +1,8 @@
+ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class PagesController {
+
+    public home({view}: HttpContextContract) {
+        return view.render('tasks/index')
+    }
+}
